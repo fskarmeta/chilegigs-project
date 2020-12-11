187ed219b38978908156a78c25e3cd58038288ab
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-12-11 04:37:03 -0300

    cambios menores

--- src/components/home/spinner.js
+++ src/components/home/spinner.js
@@ -3,16 +3,16 @@ import "./spinner.css";
 const Spinner = () => {
   return (
     <div id="trnt">
-      <div class="trnt_turntable">
-        <div class="trnt_floor"></div>
-        <div class="trnt_arm"></div>
-        <div class="trnt_vinyl">
-          <div class="trnt_wheel trnt_wheel-1"></div>
-          <div class="trnt_wheel trnt_wheel-2"></div>
-          <div class="trnt_wheel trnt_wheel-3"></div>
-          <div class="trnt_cover"></div>
-          <div class="trnt_middle"></div>
-          <div class="trnt_hole"></div>
+      <div className="trnt_turntable">
+        <div className="trnt_floor"></div>
+        <div className="trnt_arm"></div>
+        <div className="trnt_vinyl">
+          <div className="trnt_wheel trnt_wheel-1"></div>
+          <div className="trnt_wheel trnt_wheel-2"></div>
+          <div className="trnt_wheel trnt_wheel-3"></div>
+          <div className="trnt_cover"></div>
+          <div className="trnt_middle"></div>
+          <div className="trnt_hole"></div>
         </div>
       </div>
     </div>
