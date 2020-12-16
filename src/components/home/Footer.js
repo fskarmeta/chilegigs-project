ed78561fd609e06b114c61f392948748ad3242ff
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-12-16 03:46:02 -0300

    footer cambia de color con resto del home

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Context } from "../../store/appContext";
+import { useContext } from "react";
 
 const Footer = () => {
+  const { store } = useContext(Context);
   return (
     <footer>
-      <div className="bg-dark text-white">
+      <div
+        className="text-dark border-top"
+        style={{
+          backgroundColor: `${
+            !!store.home.subheader && store.home.subheader.color
+          }`,
+        }}
+      >
         <div className="container footer mt-5">
           <div className="row">
             <div className="col-md-12 text-center">
@@ -12,13 +22,19 @@ const Footer = () => {
           </div>
           <div className="row text-center mt-5">
             <div className="col-md-6">
-              <button type="button" className="btn btn-outline-info btn-lg mt-2">
+              <button
+                type="button"
+                className="btn btn-outline-info btn-lg mt-2"
+              >
                 <i className="fas fa-envelope mx-2" />
                 Correo electrónico
               </button>
             </div>
             <div className="col-md-6">
-              <button type="button" className="btn btn-outline-info btn-lg mt-2">
+              <button
+                type="button"
+                className="btn btn-outline-info btn-lg mt-2"
+              >
                 Suscribirse al Newsletter
               </button>
             </div>
@@ -39,7 +55,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-
     </footer>
   );
 };
