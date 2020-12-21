b8056317ef7fbba39e10532a4a715a99b77a2778
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-12-20 22:21:32 -0300

    al dar feedback se te redirecciona a la pagina anterior

--- src/views/gigs/feedback.js
+++ src/views/gigs/feedback.js
@@ -1,13 +1,22 @@
-import React, { useContext } from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
 import { Context } from "../../store/appContext";
 import Feedback from "../../components/gigs/feedback";
 
+const todoOk = (
+  <h4 className="font-weight-bold text-success">
+    Muchas gracias !! Volverás a la página anterior....
+  </h4>
+);
+
 const FeedbackView = () => {
   const { store } = useContext(Context);
+  const [msg, setMsg] = useState("");
+  const [error, setError] = useState(false);
+  const [ok, setOk] = useState(false);
 
   let { id } = useParams();
-
+  let history = useHistory();
   function sendFeedback(data) {
     fetch(`${store.fetchUrl}user/feedback`, {
       method: "PUT",
@@ -23,16 +32,27 @@ const FeedbackView = () => {
       .then((data) => {
         if (data.msg) {
           console.log(data.msg);
+          setMsg(data.msg);
+          setError(true);
+        } else {
+          setError(false);
+          setOk(true);
+          setTimeout(function () {
+            history.goBack();
+          }, 2000);
         }
       })
       .catch((error) => {
         console.log(error.message);
+        setMsg(error.message);
+        setError(true);
       });
   }
 
   return (
     <div className="container d-flex justify-content-center mt-5">
-      <Feedback id={id} sendFeedback={sendFeedback} />
+      {ok ? todoOk : <Feedback id={id} sendFeedback={sendFeedback} />}
+      {error ? msg : null}
     </div>
   );
 };
